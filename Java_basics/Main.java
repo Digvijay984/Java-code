58434eec01c4774c7e1df277e4145e6ec38bc504
Digvijay984 <digvijaybaraiya32@gmail.com>
2026-04-11 10:13:08 +0530

    Add three number

--- Java_basics/Main.java
+++ Java_basics/Main.java
@@ -1,10 +1,11 @@
 class Main{
 	public static void main(String[]args)
 	{
-	int a=1;
-	int b=8;
-	int c=5;
-	int Sum = a+b+c;
-	System.out.println(Sum);
+        System.out.print("The sum of these number is: ");
+	    int a=1;
+	    int b=8;
+	     int c=5;
+	     int Sum = a+b+c;
+	     System.out.println(Sum);
 	}
 }
